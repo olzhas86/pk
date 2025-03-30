a88bdcae8166e39b5d7f701402f48bdf02b39152
olzhas86 <olzhaskdyr9@gmail.com>
2025-03-30 20:32:19 +0000

    Pending changes exported from your codespace

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Mar 30 20:19:45 UTC 2025"
+#define LINUX_COMPILE_BY "olzhas0986"
+#define LINUX_COMPILE_HOST "dev"
+#define LINUX_COMPILER "Ubuntu clang version 21.0.0 (++20250329052842+e16541e1a6c0-1~exp1~20250329173033.1359), Ubuntu LLD 21.0.0"
